09eb07b2ec257749e0a1e1d2787d9909046958ba
ayun89726-byte <ayun89726@gmail.com>
2025-09-25 11:18:43 +0700

    setup awal

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Daftar barang & harga dipisah
+$barang = ["gula", "chicki", "aqua", "teh pucuk", "golda"];
+$harga  = [12000, 5000, 3000, 4000, 5000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+?>
