8d538f25a1754b17b42a2067b8d4c327012bce19
ayun89726-byte <ayun89726@gmail.com>
2025-09-25 11:21:31 +0700

    logika pembelian

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <?php
 // Daftar barang & harga dipisah
-$barang = ["gula", "chicki", "aqua", "teh pucuk", "golda"];
-$harga  = [12000, 5000, 3000, 4000, 5000];
+$barang = ["teh pucuk", "coca cola", "mizeone", "aqua", "le mineral"];
+$harga  = [12000, 18000, 13000, 10000, 15000];
 
 echo "<h1>--POLGAN MART--</h1>";
 
-?>
+// Array untuk simpan hasil belanja
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
+// Jumlah barang ada 5 (tanpa count)
+$jumlah_barang = 5;
+
+// Tentukan jumlah pembelian random (2 s/d 5)
+$jumlah_pembelian = rand(2, $jumlah_barang);
+
+// Index barang manual
+$keys = [0, 1, 2, 3, 4];
+
+for ($i = 0; $i < $jumlah_pembelian; $i++) {
+    $idx = array_rand($keys);   // pilih index random dari keys
+    $id  = $keys[$idx];         // ambil index barang
+    ($keys[$idx]);         // hapus supaya tidak double
+
+    $nama         = $barang[$id];
+    $harga_barang = $harga[$id];
+
+    $kuantitas = rand(1, 3);
+    $total     = $harga_barang * $kuantitas;
+    $grandtotal += $total;
+
+    $beli[] = [
+        "nama"   => $nama,
+        "harga"  => $harga_barang,
+        "jumlah" => $kuantitas,
+        "total"  => $total
+    ];
+}
+
